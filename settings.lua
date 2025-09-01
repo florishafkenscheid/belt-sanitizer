bafd9b6f6a18cb25472bfdd4bece4cc879d047b2
Floris Hafkenscheid <floris@hafkenscheid.com>
2025-09-01 13:11:21 +0200

    feat(settings): Add sanitizer settings

---
+++ settings.lua
@@ -0,0 +1,51 @@
+data:extend({
+    {
+        type = "bool-setting",
+        name = "belt-sanitizer-apply-fixes",
+        setting_type = "runtime-global",
+        default_value = true,
+        order = "a-a"
+    },
+    {
+        type = "bool-setting",
+        name = "belt-sanitizer-fix-pollution",
+        setting_type = "runtime-global",
+        default_value = true,
+        order = "a-b"
+    },
+    {
+        type = "bool-setting",
+        name = "belt-sanitizer-fix-biters",
+        setting_type = "runtime-global",
+        default_value = true,
+        order = "a-c"
+    },
+    {
+        type = "bool-setting",
+        name = "belt-sanitizer-peaceful-mode",
+        setting_type = "runtime-global",
+        default_value = false,
+        order = "a-d"
+    },
+    {
+        type = "bool-setting",
+        name = "belt-sanitizer-disable-expansion",
+        setting_type = "runtime-global",
+        default_value = true,
+        order = "a-e"
+    },
+    {
+        type = "bool-setting",
+        name = "belt-sanitizer-freeze-daytime",
+        setting_type = "runtime-global",
+        default_value = false,
+        order = "a-f"
+    },
+    {
+        type = "bool-setting",
+        name = "belt-sanitizer-write-diagnostics",
+        setting_type = "runtime-global",
+        default_value = true,
+        order = "a-g"
+    },
+})
